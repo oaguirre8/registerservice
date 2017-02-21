7e97d94b72c6d4ea635f91e8e3db1c2145d830cc
Richard Becker <rkbecker@uark.edu>
2017-02-21 01:43:15 -0600

    Started work on 7.2: Added EmployeeEntity.java, EmployeeFieldNames.java, EmployeeRepository.java, EmployeeRepositoryInterface.java

---
+++ src/main/java/edu/uark/models/entities/fieldnames/EmployeeFieldNames.java
@@ -0,0 +1,13 @@
+package edu.uark.models.entities.fieldnames;
+
+public class EmployeeFieldNames {
+	public static final String RECORD_ID = "recordid";
+	public static final String FIRST_NAME = "firstname";
+	public static final String LAST_NAME = "lastname";
+	public static final String EMPLOYEE_ID = "employeeid";
+	public static final String ACTIVE = "active";
+	public static final String ROLE = "role";
+	public static final String MANAGER = "manager";
+	public static final String PASSWORD = "password";
+	public static final String CREATED_ON = "createdon";
+}
